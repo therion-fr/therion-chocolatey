8b2ad93eb194dd5dd7d83db68bc8bbdf3c6b8cb7
Tristan Carel <tristan.carel@protonmail.com>
2025-02-03 10:50:54 +0100

    take gep13 comments into account

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,21 +2,17 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/therion/therion/releases/download/v6.3.1/therion-setup-v6.3.1.exe'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'therion*'
 
   checksum      = 'DEBF19F9733DB4B441E3FDF63EA0CE3E64D26E8BF4A36E3D74249D0BCD5B6C5E'
   checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
